180f027edaadf3b446354e4388778cbe11fdda20
Ps Prosen <psprosen@hotmail.com>
2025-03-24 21:03:46 +0000

    Manual deploy trigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,11 @@
-/**
- * Application component
- *
- * To contain application wide settings, routes, state, etc.
- */
-
-import React from "react";
-
-import About from "./Components/About";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Home from "./Components/Home";
-import Portfolio from "./Components/Portfolio";
-
-import "./styles.css";
-
-/**
- * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
- * components that need that information.
- *
- * Update the values below with your information.
- *
- * If you don't have one of the social sites listed, leave it as an empty string.
- */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Prosen Dev",
+  title: "Digital Marketing Specialist & Open-Source Enthusiast",
+  email: "prosen@example.com",
+  gitHub: "PsProsen-Dev",
+  instagram: "prosen_ig",
+  linkedIn: "prosen-linkedin",
+  medium: "prosen-medium",
+  twitter: "prosen_twitter",
+  youTube: "prosen_youtube",
 };
-
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
-
-const App = () => {
-  return (
-    <div id="main">
-      <Header />
-      <Home name={siteProps.name} title={siteProps.title} />
-      <About />
-      <Portfolio />
-      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
-    </div>
-  );
-};
-
-export default App;
